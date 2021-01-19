7b1b4df2f67c0778345c00fb0e47c2dd823fcfe9
Carlos Mion <mion.carlos23@gmail.com>
2021-01-19 11:19:42 -0300

    added bklogo component

---
+++ src/components/molecules/BKLogo/BKLogo.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { SectionItemType } from '../../../../types';
+import { Text } from 'components/atoms/Text';
+import { Link } from 'react-router-dom';
+
+export function BKLogo() {
+  return (
+    <Link to={'/'}>
+      <Container>
+        <Image
+          src={process.env.PUBLIC_URL + `/images/bk-logo.png`}
+          alt="Burguer King logo"
+        />
+        <Title color="red">Menu</Title>
+        <Marker />
+      </Container>
+    </Link>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  padding-top: 16px;
+  height: 100%;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  background-color: ${({ theme }) => theme.colors.silverDark};
+`;
+
+const Image = styled.img`
+  display: flex;
+  width: 102px;
+  height: 64px;
+  border-radius: 6px;
+`;
+
+const Title = styled(Text.UpperCaseBoldLarge)`
+  text-align: center;
+  width: 100%;
+  display: -webkit-box;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+  text-overflow: visible;
+  overflow: visible;
+`;
+
+const Marker = styled.div`
+  background-color: ${({ theme }) => theme.colors.red};
+  border-radius: 4000px;
+  height: 4px;
+  width: 64px;
+`;
---
+++ src/components/molecules/BKLogo/index.ts
@@ -0,0 +1,1 @@
+export { BKLogo } from './BKLogo';
