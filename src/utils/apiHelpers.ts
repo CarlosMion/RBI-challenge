0a2f214d16a1b0586190f9f0b0220f35899e2b6a
Carlos Mion <mion.carlos23@gmail.com>
2021-01-20 08:12:12 -0300

    added helper to get items to be shown in grid

--- src/utils/apiHelpers.ts
+++ src/utils/apiHelpers.ts
@@ -1,5 +1,15 @@
-import { SectionItemType } from "../../types";
+import { ItemType, MenuItemOptionsType, SectionItemType } from "../../types";
 
 
-export const getMenuSectionInfo = (referenceToFind: string, sections: Array<SectionItemType>) =>
- sections.find((section) => section._id === referenceToFind);
+export const getMenuSectionInfo = (referenceToFind: string, sections: Array<SectionItemType>) => referenceToFind !== undefined ? 
+ sections.find((section) => section._id === referenceToFind) : referenceToFind;
+
+const sectionIncludes = (item: ItemType, options: Array<MenuItemOptionsType>) => 
+options.find((option) => option._ref === item._id);
+    
+
+export const getItemsToBeShown = (currentSection: SectionItemType, items?: Array<ItemType>) => 
+     items?.filter((item) => 
+        !!sectionIncludes(item, currentSection.options)
+    );
+
