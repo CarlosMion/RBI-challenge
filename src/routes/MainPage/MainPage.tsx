b2b141d1d53aa1c0a6e99d0e4bbaee20c87a1649
Carlos Mion <mion.carlos23@gmail.com>
2021-01-20 14:53:13 -0300

    set to repeat sections when no section was selected

--- src/routes/MainPage/MainPage.tsx
+++ src/routes/MainPage/MainPage.tsx
@@ -6,12 +6,20 @@ import { CarouselMenu } from 'components/organisms/CarouselMenu';
 import { useSections } from 'api/sections';
 import { useItems } from 'api/items';
 import { useParams } from 'react-router-dom';
-import { getItemsToBeShown, getMenuSectionInfo } from 'utils/apiHelpers';
+import {
+  getItemsToBeShown,
+  getMenuSectionInfo,
+  getSectionItemsToBeShown,
+} from 'utils/apiHelpers';
 import { ItemsGrid } from 'components/organisms/ItemsGrid';
 import { Scrollbar, ScrollbarAutoHide } from 'components/styles';
+import GridSkeleton from 'components/molecules/Skeletons/GridSkeleton';
+import { useMenu } from 'api/menu';
 
 export default function MainPage() {
   const { selectedSection } = useParams<{ selectedSection: string }>();
+
+  const { menuOptions, isFetching: menuIsFetching } = useMenu();
 
   const { sections, isFetching: sectionsAreFetching } = useSections();
 
@@ -20,20 +28,18 @@ export default function MainPage() {
   const isLoadingItems = sectionsAreFetching && itemsAreFetching;
 
   const currentSection =
-    sections && getMenuSectionInfo(selectedSection, sections); // if current section is undefined, load sections
+    sections && getMenuSectionInfo(selectedSection, sections);
 
   const itemsToBeShown = currentSection
     ? getItemsToBeShown(currentSection, items)
-    : [];
-
-  console.log(itemsToBeShown);
+    : getSectionItemsToBeShown(menuOptions, sections);
 
   return (
     <MainTemplate>
       <CarouselMenu />
       <Container>
         {isLoadingItems ? (
-          <div>loading</div>
+          <GridSkeleton />
         ) : (
           <ItemsGrid items={itemsToBeShown} />
         )}
@@ -43,7 +49,7 @@ export default function MainPage() {
 }
 
 export const Container = styled.div`
-  padding-top: 156px;
+  padding-top: 116px;
   background-color: ${({ theme }) => theme.colors.cloud};
   width: 100%;
   height: 100%;
@@ -53,4 +59,8 @@ export const Container = styled.div`
   justify-content: center;
   ${Scrollbar}
   ${ScrollbarAutoHide}
+
+  @media only screen and (min-width: 481px) {
+    padding-top: 156px;
+  }
 `;
--- src/utils/apiHelpers.ts
+++ src/utils/apiHelpers.ts
@@ -1,8 +1,8 @@
 import { ItemType, MenuItemOptionsType, SectionItemType } from "../../types";
 
 
-export const getMenuSectionInfo = (referenceToFind: string, sections: Array<SectionItemType>) => referenceToFind !== undefined ? 
- sections.find((section) => section._id === referenceToFind) : referenceToFind;
+export const getMenuSectionInfo = (referenceToFind: string, sections?: Array<SectionItemType>) => referenceToFind !== undefined ? 
+ sections?.find((section) => section._id === referenceToFind) : referenceToFind;
 
 const sectionIncludes = (item: ItemType, options: Array<MenuItemOptionsType>) => 
 options.find((option) => option._ref === item._id);
@@ -12,4 +12,18 @@ export const getItemsToBeShown = (currentSection: SectionItemType, items?: Array
      items?.filter((item) => 
         !!sectionIncludes(item, currentSection.options)
     );
+
+export const getSectionItemsToBeShown = (menuOptions?: Array<MenuItemOptionsType>, sections?: Array<SectionItemType>) => {
+    
+    const sectionsToBeShown = menuOptions?.map((menuOption) => getMenuSectionInfo(menuOption._ref, sections))
+    
+    const itemsToBeShown = sectionsToBeShown?.map((section) => ({
+        description: section?.description,
+        image: section?.image,
+        name: section?.name,
+        _id: section?._id
+    } as ItemType));
+
+    return itemsToBeShown;
+}
 
